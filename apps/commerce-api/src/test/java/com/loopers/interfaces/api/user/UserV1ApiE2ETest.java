1d1bd5b8fae12bf6ebea3b570d18697c18a2ee1f
chanho <mwma91@gmail.com>
2025-08-01 01:01:11 +0900

    fix: 사용자 테스트 오류 수정
    - 조회를 위한 회원가입을 UserFacade로 처리
    - @NullAndEmptySource 의 잘못된 사용 제거
    - 회원가입 시 잘못된 성별 파라미터 사용 수정 ("M" -> "male")

--- apps/commerce-api/src/test/java/com/loopers/interfaces/api/user/UserV1ApiE2ETest.java
+++ apps/commerce-api/src/test/java/com/loopers/interfaces/api/user/UserV1ApiE2ETest.java
@@ -1,5 +1,6 @@
 package com.loopers.interfaces.api.user;
 
+import com.loopers.application.user.UserFacade;
 import com.loopers.domain.user.UserGender;
 import com.loopers.interfaces.api.ApiResponse;
 import com.loopers.utils.DatabaseCleanUp;
@@ -9,12 +10,13 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+
+import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -31,6 +33,9 @@ class UserV1ApiE2ETest {
      * - [o]  내 정보 조회에 성공할 경우, 해당하는 유저 정보를 응답으로 반환한다.
      * - [o]  존재하지 않는 ID 로 조회할 경우, `404 Not Found` 응답을 반환한다.
      */
+
+    @Autowired
+    private UserFacade userFacade;
 
     @Autowired
     private TestRestTemplate testRestTemplate;
@@ -57,7 +62,7 @@ class UserV1ApiE2ETest {
                     {
                         "username": "mwma91",
                         "email": "test@gmail.com",
-                        "gender": "M",
+                        "gender": "male",
                         "birth": "2000-01-01"
                     }
                     """;
@@ -82,7 +87,6 @@ class UserV1ApiE2ETest {
 
         @DisplayName("회원 가입 시 요청 값이 올바르지 않을 경우 `400 Bad Request` 응답을 반환한다.")
         @ParameterizedTest(name = "{index} - {0}")
-        @NullAndEmptySource
         @MethodSource("com.loopers.interfaces.api.user.param.SignupRequestParam#invalidRequests")
         void returnsBadRequest_whenParameterIsInvalid(String testName, String requestBody, HttpStatus expectedStatus) {
             // arrange
@@ -114,7 +118,12 @@ class UserV1ApiE2ETest {
         @Test
         void returnsUserInfo_whenGetMyInfoIsSuccessful() {
             // arrange
-            UserV1Dto.UserResponse created = signUpUser("mwma91", "test@gmail.com", "M", "2000-01-01");
+            UserV1Dto.UserResponse created = userFacade.signUp(new UserV1Dto.SignupRequest(
+                    "mwma91",
+                    "test@gmail.com",
+                    UserGender.M,
+                    LocalDate.of(2000, 1, 1))
+            );
 
             // act
             HttpHeaders httpHeaders = new HttpHeaders();
@@ -132,31 +141,6 @@ class UserV1ApiE2ETest {
                     () -> assertThat(response.getBody().data().username()).isEqualTo(created.username()),
                     () -> assertThat(response.getBody().data().gender()).isEqualTo(created.gender())
             );
-        }
-
-        private UserV1Dto.UserResponse signUpUser(String name, String email, String gender, String birth) {
-            String signupRequest = """
-                    {
-                        "username": "%s",
-                        "email": "%s",
-                        "gender": "%s",
-                        "birth": "%s"
-                    }
-                    """.formatted(name, email, gender, birth);
-
-            HttpHeaders httpHeaders = new HttpHeaders();
-            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-
-            ParameterizedTypeReference<ApiResponse<UserV1Dto.UserResponse>> responseType = new ParameterizedTypeReference<>() {
-            };
-            ResponseEntity<ApiResponse<UserV1Dto.UserResponse>> response =
-                    testRestTemplate.exchange("/api/v1/users", HttpMethod.POST, new HttpEntity<>(signupRequest, httpHeaders), responseType);
-
-            assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-            assertThat(response.getBody()).isNotNull();
-            assertThat(response.getBody().data()).isNotNull();
-
-            return response.getBody().data();
         }
 
         @DisplayName("존재하지 않는 ID 로 조회할 경우, `404 Not Found` 응답을 반환한다.")
