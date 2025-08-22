9c6a8082a1a06c1cee45686fd15ddb1ef5d11541
chanho <mwma91@gmail.com>
2025-08-22 21:19:11 +0900

    feat(payment): pg 결제 요청에 재시도, 써킷브레이커 적용

--- apps/commerce-api/src/main/java/com/loopers/domain/payment/processor/CardPaymentProcessor.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/payment/processor/CardPaymentProcessor.java
@@ -6,7 +6,8 @@ import com.loopers.domain.payment.PaymentMethod;
 import com.loopers.infrastructure.payment.PgClient;
 import com.loopers.interfaces.api.payment.CardType;
 import com.loopers.interfaces.api.payment.PaymentV1Dto;
-import feign.FeignException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -38,6 +39,8 @@ public class CardPaymentProcessor implements PaymentProcessor {
     }
 
     @Override
+    @CircuitBreaker(name = "pg-api", fallbackMethod = "processPaymentFallback")
+    @Retry(name = "pg-api")
     public void processPayment(PaymentEntity payment) {
         PaymentV1Dto.Request request = PaymentV1Dto.Request.create(
                 payment.getOrderId(),
@@ -47,21 +50,21 @@ public class CardPaymentProcessor implements PaymentProcessor {
                 callbackUrl
         );
 
-        try {
-            PaymentV1Dto.TransactionResponse response = pgClient.createPaymentRequest(pgUserId, request);
-            log.info("Payment request response: {}", response);
+        PaymentV1Dto.TransactionResponse response = pgClient.createPaymentRequest(pgUserId, request);
+        log.info("Payment request response: {}", response);
 
-            if (response.isSuccess()) {
-                CardPaymentEntity cardPayment = (CardPaymentEntity) payment;
-                cardPayment.setTransactionKey(response.data().transactionKey());
-                return;
-            } else {
-                log.info("Payment request failed: {}", response.meta());
-                payment.fail();
-            }
-        } catch (FeignException e) {
-            log.error("Payment request failed with FeignException:", e);
-            payment.fail();
+        if (response.isSuccess()) {
+            CardPaymentEntity cardPayment = (CardPaymentEntity) payment;
+            cardPayment.setTransactionKey(response.data().transactionKey());
+            return;
         }
+
+        log.info("Payment request failed: {}", response.meta());
+        payment.fail();
+    }
+
+    private void processPaymentFallback(PaymentEntity payment, Throwable t) {
+        log.error("PG 결제 요청 최종 실패. Fallback 실행. orderId: {}, error: {}", payment.getOrderId(), t.getMessage());
+        payment.fail();
     }
 }
