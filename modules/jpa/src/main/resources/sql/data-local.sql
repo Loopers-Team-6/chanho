8e02590336b76fbb389ef8aacce2ff805adc3a0c
chanho <mwma91@gmail.com>
2025-09-06 01:25:15 +0900

    feat: data-local.sql 추가
    - 샘플 데이터 insert

---
+++ modules/jpa/src/main/resources/sql/data-local.sql
@@ -0,0 +1,8 @@
+insert into members (birth, id, username, email, gender)
+values ('1990-01-01', 1, 'testuser', 'test@test.com', 'MALE');
+
+insert into brands (id, brand_name)
+values (1, '샘플 브랜드');
+
+insert into products (id, price, stock, brand_id, name)
+values (1, 1000, 10, 1, 'Sample Product');
