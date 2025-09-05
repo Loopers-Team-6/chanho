caea778391dba2e0824d72106019eb034e42be06
chanho <mwma91@gmail.com>
2025-09-06 01:23:41 +0900

    fix: 좋아요 api 엔드포인트 추가

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/product/ProductV1ApiController.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/product/ProductV1ApiController.java
@@ -8,10 +8,7 @@ import com.loopers.support.error.ErrorType;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -40,8 +37,9 @@ public class ProductV1ApiController implements ProductV1ApiSpec {
         return ApiResponse.success(productFacade.getProducts(pageable, Long.valueOf(userId)));
     }
 
+    @PostMapping("/{productId}/like")
     @Override
-    public ApiResponse<Object> addLike(Long productId, HttpServletRequest request) {
+    public ApiResponse<Object> addLike(@PathVariable Long productId, HttpServletRequest request) {
         String userId = request.getHeader("X-USER-ID");
         if (userId == null) {
             throw new CoreException(ErrorType.BAD_REQUEST, "X-USER-ID header is required.");
@@ -51,8 +49,9 @@ public class ProductV1ApiController implements ProductV1ApiSpec {
         return ApiResponse.success();
     }
 
+    @DeleteMapping("/{productId}/like")
     @Override
-    public ApiResponse<Object> removeLike(Long productId, HttpServletRequest request) {
+    public ApiResponse<Object> removeLike(@PathVariable Long productId, HttpServletRequest request) {
         String userId = request.getHeader("X-USER-ID");
         if (userId == null) {
             throw new CoreException(ErrorType.BAD_REQUEST, "X-USER-ID header is required.");
